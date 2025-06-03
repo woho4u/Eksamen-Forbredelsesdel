70af8bb8c18b78b75bf7be6fccd6620cf6da7936
woho4u <okebedrift@gmail.com>
2025-06-03 11:25:42 +0200

    add the recipe app and view as a url path

--- eksamenForbredelse_backend/urls.py
+++ eksamenForbredelse_backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from recipes.views import RecipeViewSet
+
+router = DefaultRouter()
+router.register(r'recipes', RecipeViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
