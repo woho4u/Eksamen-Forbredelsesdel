b550dc6a3dbe3a0c3e868d792471a7798467f1dc
woho4u <okebedrift@gmail.com>
2025-06-03 11:54:59 +0200

    add django-filter parameters

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,13 @@
 from rest_framework import viewsets
 from .models import Recipe
 from .serializers import RecipeSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import RecipeFilter
+from rest_framework.filters import SearchFilter
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_class = RecipeFilter
+    search_fields = ['title', 'category', 'flavor_profiles']  # keep search for text if desired
