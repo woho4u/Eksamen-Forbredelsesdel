f9095ea3a5eea47a44018b32498c4a2524006390
woho4u <okebedrift@gmail.com>
2025-06-03 11:00:48 +0200

    create recipe model profile

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Recipe (models.Model):
+    title = models.CharField(max_length=255)
+    recipe = models.CharField(max_length=5000)
+    category = models.CharField(max_length=100)
+    flavor_profiles = models.JSONField(default=list, help_text='A list containing the flavor profiler og the meal')
+
+    def __str__(self):
+        return self.title
