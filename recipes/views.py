1281732f500ed31cd4d23a378180ef4ff359e726
woho4u <okebedrift@gmail.com>
2025-06-03 11:25:51 +0200

    create the view

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
